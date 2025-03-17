ec0a7fba906119a3a0436be4e6e93a42db208ed2
IxioJo <117433798+IxioJo@users.noreply.github.com>
2025-03-17 14:18:00 +0100

    Update mcp41010.h

--- components/mcp41010/mcp41010.h
+++ components/mcp41010/mcp41010.h
@@ -1,12 +1,16 @@
+#pragma once
+
 #include "esphome/core/component.h"
 #include "esphome/components/spi/spi.h"
+#include "mcp41010_component.h"
 
 namespace esphome {
 namespace mcp41010 {
 
-class MCP41010 : public Component, public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_1MHZ> {
+class MCP41010 : public spi::SPIDevice<spi::BIT_ORDER_MSB_FIRST, spi::CLOCK_POLARITY_LOW, spi::CLOCK_PHASE_LEADING, spi::DATA_RATE_1MHZ> {
  public:
-  void setup() override;
+  MCP41010(MCP41010Component *parent) { parent->register_mcp(this); }
+  void setup();
   void set_value(uint8_t value);
 };
 
