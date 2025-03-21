343cf9d94b25cd838fb3c54aac98d6f607fe3f1a
IxioJo <117433798+IxioJo@users.noreply.github.com>
2025-03-21 11:33:47 +0100

    Update mcp41010.h

--- components/mcp41010/mcp41010.h
+++ components/mcp41010/mcp41010.h
@@ -2,16 +2,29 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/spi/spi.h"
-#include "mcp41010_component.h"  // Maintenant, on peut inclure ce fichier
 
 namespace esphome {
 namespace mcp41010 {
 
-class MCP41010 {
+class MCP41010 : public Component, public spi::SPIDevice {
  public:
-  MCP41010(MCP41010Component *parent) { parent->register_mcp(this); }
-  
-  void set_value(uint8_t value);
+  MCP41010(uint8_t cs_pin) { 
+    this->cs_pin_ = cs_pin;
+  }
+
+  void setup() override {
+    pinMode(this->cs_pin_, OUTPUT);
+    digitalWrite(this->cs_pin_, HIGH);
+  }
+
+  void set_value(uint8_t value) {
+    digitalWrite(this->cs_pin_, LOW);
+    this->write_byte(value);
+    digitalWrite(this->cs_pin_, HIGH);
+  }
+
+ protected:
+  uint8_t cs_pin_;
 };
 
 }  // namespace mcp41010
