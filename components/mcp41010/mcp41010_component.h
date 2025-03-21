55b3ac6a9fafc0d7e5348b540378b048fc1338a7
IxioJo <117433798+IxioJo@users.noreply.github.com>
2025-03-21 11:23:54 +0100

    Update mcp41010_component.h

--- components/mcp41010/mcp41010_component.h
+++ components/mcp41010/mcp41010_component.h
@@ -1,22 +1,20 @@
 #pragma once
 
 #include "esphome/core/component.h"
+#include "esphome/components/spi/spi.h"
 #include <vector>
 
 namespace esphome {
 namespace mcp41010 {
 
-class MCP41010;  // Forward declaration
+class MCP41010;  // Déclaration anticipée
 
 class MCP41010Component : public Component {
  public:
+  void setup() override;
+  void dump_config() override;
+
   void register_mcp(MCP41010 *mcp) { mcps_.push_back(mcp); }
-  
- private:
-  std::vector<MCP41010 *> mcps_;
-};
 
-}  // namespace mcp41010
-}  // namespace esphome
-
-#include "mcp41010.h"  // Inclusion après déclaration de MCP41010Component
+ protected:
+  std::vector<MCP41010 *> mcps_;  // Util
