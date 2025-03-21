3e63a975e82cd683f77210efe7968ce31121cb9e
IxioJo <117433798+IxioJo@users.noreply.github.com>
2025-03-21 11:24:15 +0100

    Update mcp41010.h

--- components/mcp41010/mcp41010.h
+++ components/mcp41010/mcp41010.h
@@ -2,22 +2,16 @@
 
 #include "esphome/core/component.h"
 #include "esphome/components/spi/spi.h"
+#include "mcp41010_component.h"  // Maintenant, on peut inclure ce fichier
 
 namespace esphome {
 namespace mcp41010 {
 
-// Forward declaration
-class MCP41010Component;
-
-class MCP41010 : public Component, public spi::SPIDevice {
+class MCP41010 {
  public:
-  MCP41010(MCP41010Component *parent);  // Déplacement du constructeur vers le .cpp
+  MCP41010(MCP41010Component *parent) { parent->register_mcp(this); }
+  
   void set_value(uint8_t value);
-  void setup() override;
-  void dump_config() override;
-
- private:
-  MCP41010Component *parent_;
 };
 
 }  // namespace mcp41010
